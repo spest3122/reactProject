7168c5e3f25d0885866aa080561780c7ab972f11
spest3122 <spest3122@gmail.com>
2021-10-03 15:33:49 +0800

    今日事項 1. 完成表格式邏輯(尚缺詳細與頁碼顯示於中間)

--- src/views/main/member/column/index.jsx
+++ src/views/main/member/column/index.jsx
@@ -3,20 +3,56 @@ import { getUserName } from 'api'
 
 const Column = () => {
     const [list, setList] = useState([])
-    const [total, setTotal] = useState(0)
+    const [pageConfig, setPageConfig] = useState({
+        total: 0,
+        page: 0,
+        size: 10,
+        pageList: [],
+    })
     useEffect(() => {
-        async function getUserList() {
-            let res = await getUserName({ page: 0, size: 10 })
-            let listData = res.data.data.content
-            let totalNumber = res.data.data.total
-            setTotal(totalNumber)
-            setList(listData)
-        }
         getUserList()
-    }, [])
+    }, [pageConfig.page])
+
+    const getUserList = async () => {
+        let res = await getUserName({
+            page: pageConfig.page,
+            size: pageConfig.size,
+            pageList: [],
+        })
+        let listData = res.data.data.content
+        let totalNumber = res.data.data.total
+        let pageList = Array.from(
+            { length: Math.ceil(totalNumber / 10) },
+            (_, i) => i + 1
+        )
+        setPageConfig((prev) => ({
+            ...prev,
+            total: totalNumber,
+            pageList: pageList,
+        }))
+        setList(listData)
+    }
+
+    const changePage = (move) => {
+        let page = pageConfig.page
+        let list = pageConfig.pageList
+        if (move === 'next') {
+            page += 1
+        } else if (move === 'prev') {
+            page -= 1
+        } else {
+            page = move
+        }
+
+        if (page >= list[list.length - 1] || page < 0) {
+            return
+        }
+
+        setPageConfig((prev) => ({ ...prev, page: page }))
+    }
 
     return (
-        <main className="ml-2 mt-4">
+        <main className="ml-4 mr-4 mt-4">
             <h1>會員管理(列表式)</h1>
             <table className="w-full text-left mt-3">
                 <thead className="border-b-2 border-black">
@@ -40,16 +76,35 @@ const Column = () => {
                     ))}
                 </tbody>
             </table>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center mt-4">
                 <ul className="flex">
-                    <li>{'<'}</li>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>{'>'}</li>
+                    <li
+                        className="mr-2 leading-5"
+                        onClick={() => changePage('prev')}
+                    >
+                        {'<'}
+                    </li>
+                    {pageConfig.pageList.map((item, index) => (
+                        <li
+                            className={`pl-1 leading-5 pr-1 ${
+                                pageConfig.page === index
+                                    ? 'text-2xl text-blue-500'
+                                    : ''
+                            }`}
+                            key={'page' + index}
+                            onClick={() => changePage(index)}
+                        >
+                            {item}
+                        </li>
+                    ))}
+                    <li
+                        className="leading-5 ml-2"
+                        onClick={() => changePage('next')}
+                    >
+                        {'>'}
+                    </li>
                 </ul>
-                <p>{`共 ${total} 筆`}</p>
+                <p>{`共 ${pageConfig.total} 筆`}</p>
             </div>
         </main>
     )
